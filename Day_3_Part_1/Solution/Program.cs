e78120a4e1762428b41850c15aacbd3a07c8a007
Richard Hoekstra <me@richardhoekstra.com>
2020-08-21 21:34:40 +0200

    Initializes day 3

---
+++ Day_3_Part_1/Solution/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Solution
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static int ExampleFunction(int x)
+        {
+            return x + 1;
+        }
+    }
+}
