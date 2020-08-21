e0a372cb28d17d55c1016b25e306875e62c8e29f
Richard Hoekstra <me@richardhoekstra.com>
2020-08-21 22:49:51 +0200

    Solves day 3 part 1

--- Day_3_Part_1/Solution.Test/UnitTest1.cs
+++ Day_3_Part_1/Solution.Test/UnitTest1.cs
@@ -17,14 +17,13 @@ namespace Solution.Test
         [Test]
         public void ExampleTest()
         {
-            var testCases = new Dictionary<int, int>
+            var testCases = new Dictionary<string, int>
             {
-                {1, 2},
-                {2, 3},
-                {3, 4},
+                {"R75,D30,R83,U83,L12,D49,R71,U7,L72\nU62,R66,U55,R34,D71,R55,D58,R83", 159},
+                {"R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\nU98,R91,D20,R16,D67,R40,U7,R15,U6,R7", 135},
             };
             
-            foreach (KeyValuePair<int, int> testCase in testCases)
+            foreach (KeyValuePair<string, int> testCase in testCases)
             {
                 var X = testCase.Key;
                 var Y = testCase.Value;
--- Day_3_Part_1/Solution/Program.cs
+++ Day_3_Part_1/Solution/Program.cs
@@ -4,16 +4,91 @@ using System.Linq;
 
 namespace Solution
 {
+
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string input = System.IO.File.ReadAllText("input.txt");
+            Console.WriteLine($"Closest: {Program.ExampleFunction(input)}");
         }
 
-        public static int ExampleFunction(int x)
+        public static int ExampleFunction(string paths)
         {
-            return x + 1;
+            var inter = Program.PathsIntersections(paths);
+            return Program.IntersectionClosestToCenter(inter);
+        }
+
+        public static int IntersectionClosestToCenter(HashSet<(int, int)> intersections)
+        {   
+            // ALTERNATIVE: Change the intersections to a list of distances to the center 
+            // and just use <List>.Min()
+
+            bool initialized = false;
+            int min_manhattan_distance = 0;
+            foreach (var intersection in intersections)
+            {
+                var manhattan_distance = Math.Abs(intersection.Item1) + Math.Abs(intersection.Item2);
+                if(!initialized){
+                    min_manhattan_distance = manhattan_distance;
+                    initialized = true;
+                } else {
+                    if(manhattan_distance < min_manhattan_distance)
+                    {
+                        min_manhattan_distance = manhattan_distance;
+                    }
+                }
+            }
+
+            return min_manhattan_distance;
+        }
+
+        public static HashSet<(int, int)> PathsIntersections(string _paths)
+        {
+            string[] paths = _paths.Split('\n');
+            
+            var intersections = Program.PathToSet(paths[0]);
+            for (int i = 1; i < paths.Length; i++)
+            {
+                intersections.IntersectWith(Program.PathToSet(paths[i]));
+            }
+            return intersections;
+        }
+
+        public static HashSet<(int,int)> PathToSet(string path)
+        {
+            var bitmap = new HashSet<(int,int)>();
+
+            var currLocation = (x: 0, y: 0);
+
+            var vectors = path.Split(",");
+            foreach (var v in vectors)
+            {
+                char direction = v[0];
+                int magnitude = Int32.Parse(v.Substring(1));
+                for (int i = 0; i < magnitude; i++)
+                {
+                    switch (direction)
+                    {
+                        case 'L':
+                            currLocation.x--;
+                            break;
+                        case 'R':
+                            currLocation.x++;
+                            break;
+                        case 'U':
+                            currLocation.y++;
+                            break;
+                        case 'D':
+                            currLocation.y--;
+                            break;
+                        default:
+                            throw new System.InvalidProgramException("Invalid direction.");
+                    }
+                    bitmap.Add(currLocation);
+                }
+            }
+            return bitmap;
         }
     }
 }
