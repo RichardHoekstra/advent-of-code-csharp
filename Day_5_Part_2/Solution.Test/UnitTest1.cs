d9e8e85ef52b056909f9b4379fb306b10ce38276
Richard Hoekstra <me@richardhoekstra.com>
2020-08-27 21:51:41 +0200

    style: :art: Format document

--- Day_5_Part_2/Solution.Test/UnitTest1.cs
+++ Day_5_Part_2/Solution.Test/UnitTest1.cs
@@ -47,23 +47,23 @@ namespace Solution.Test
                 {"2,4,4,5,99,0", "2,4,4,5,99,9801"},
                 {"1,1,1,4,99,5,6,0,99", "30,1,1,4,2,5,6,0,99"}
             };
-            
+
             foreach (KeyValuePair<string, string> testCase in testCases)
             {
                 var X = testCase.Key;
                 var Y = testCase.Value;
-                
+
                 var computer = new IntcodeComputer(X);
                 computer.Execute();
                 var result = computer.MemoryDump();
-                Assert.IsTrue(result==Y, $"Function({X} should return {Y}, function returned {result}");
+                Assert.IsTrue(result == Y, $"Function({X} should return {Y}, function returned {result}");
             }
         }
 
         [Test]
         public void LargerExampleTest()
         {
-            
+
             var testCases = new List<(string, int, int)>
             {
                 // Is input smaller than 8?
@@ -71,9 +71,9 @@ namespace Solution.Test
                 // Is input equal to 8?
                 ("3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99", 8, 1000), 
                 // Is input bigger than 8?
-                ("3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99", 9, 1001), 
-            }; 
-            
+                ("3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99", 9, 1001),
+            };
+
             foreach ((string, int, int) testCase in testCases)
             {
                 var program = testCase.Item1;
@@ -84,8 +84,8 @@ namespace Solution.Test
                 computer.TestInput = testInput;
                 computer.Execute();
                 var result = computer.MemoryDump();
-                Assert.IsTrue(result!="");
-                Assert.IsTrue(computer.TestOutput==expectedTestOutput, $"Function({program}) should return {expectedTestOutput}, function returned {computer.TestOutput}");
+                Assert.IsTrue(result != "");
+                Assert.IsTrue(computer.TestOutput == expectedTestOutput, $"Function({program}) should return {expectedTestOutput}, function returned {computer.TestOutput}");
             }
         }
 
@@ -95,22 +95,22 @@ namespace Solution.Test
             var testCases = new List<(string, int, int)>
             {
                 // Is input equal to 8?
-                ("3,9,8,9,10,9,4,9,99,-1,8", 8, 1), 
+                ("3,9,8,9,10,9,4,9,99,-1,8", 8, 1),
                 ("3,9,8,9,10,9,4,9,99,-1,8", 7, 0), 
 
                 // Is input less than 8?
-                ("3,9,7,9,10,9,4,9,99,-1,8", 7, 1), 
+                ("3,9,7,9,10,9,4,9,99,-1,8", 7, 1),
                 ("3,9,7,9,10,9,4,9,99,-1,8", 8, 0),
 
                 // Is input equal to 8? (Immediate mode)
-                ("3,3,1108,-1,8,3,4,3,99", 8, 1), 
+                ("3,3,1108,-1,8,3,4,3,99", 8, 1),
                 ("3,3,1108,-1,8,3,4,3,99", 7, 0),
 
                 // Is input less than 8? (Immediate mode)
-                ("3,3,1107,-1,8,3,4,3,99", 7, 1), 
+                ("3,3,1107,-1,8,3,4,3,99", 7, 1),
                 ("3,3,1107,-1,8,3,4,3,99", 8, 0),
-            }; 
-            
+            };
+
             foreach ((string, int, int) testCase in testCases)
             {
                 var program = testCase.Item1;
@@ -121,8 +121,8 @@ namespace Solution.Test
                 computer.TestInput = testInput;
                 computer.Execute();
                 var result = computer.MemoryDump();
-                Assert.IsTrue(result!="");
-                Assert.IsTrue(computer.TestOutput==expectedTestOutput, $"Function({program} should return {testInput}, function returned {computer.TestOutput}");
+                Assert.IsTrue(result != "");
+                Assert.IsTrue(computer.TestOutput == expectedTestOutput, $"Function({program} should return {testInput}, function returned {computer.TestOutput}");
             }
         }
 
@@ -131,12 +131,12 @@ namespace Solution.Test
         {
             var testCases = new List<(string, int, int)>
             {
-                ("3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9", 0, 0), 
-                ("3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9", 1, 1), 
-                ("3,3,1105,-1,9,1101,0,0,12,4,12,99,1", 0, 0), 
-                ("3,3,1105,-1,9,1101,0,0,12,4,12,99,1", 1, 1), 
-            }; 
-            
+                ("3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9", 0, 0),
+                ("3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9", 1, 1),
+                ("3,3,1105,-1,9,1101,0,0,12,4,12,99,1", 0, 0),
+                ("3,3,1105,-1,9,1101,0,0,12,4,12,99,1", 1, 1),
+            };
+
             foreach ((string, int, int) testCase in testCases)
             {
                 var program = testCase.Item1;
@@ -147,8 +147,8 @@ namespace Solution.Test
                 computer.TestInput = testInput;
                 computer.Execute();
                 var result = computer.MemoryDump();
-                Assert.IsTrue(result!="");
-                Assert.IsTrue(computer.TestOutput==expectedTestOutput, $"Function({program} should return {testInput}, function returned {computer.TestOutput}");
+                Assert.IsTrue(result != "");
+                Assert.IsTrue(computer.TestOutput == expectedTestOutput, $"Function({program} should return {testInput}, function returned {computer.TestOutput}");
             }
         }
 
@@ -157,9 +157,9 @@ namespace Solution.Test
         {
             var computer = new IntcodeComputer("1002,4,3,4,33");
             var (OPCODE, MODES) = computer.ReadOpCode(0);
-            Assert.IsTrue(OPCODE==IntcodeComputer.OpCode.MULTIPLY);
-            Assert.IsTrue(MODES[0]==IntcodeComputer.Mode.POSITION);
-            Assert.IsTrue(MODES[1]==IntcodeComputer.Mode.IMMEDIATE);
+            Assert.IsTrue(OPCODE == IntcodeComputer.OpCode.MULTIPLY);
+            Assert.IsTrue(MODES[0] == IntcodeComputer.Mode.POSITION);
+            Assert.IsTrue(MODES[1] == IntcodeComputer.Mode.IMMEDIATE);
         }
     }
 }
